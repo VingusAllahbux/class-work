0478643380de1f7a8f4552ccaa962db00ebd7c69
VingusAllahbux <152180739+VingusAllahbux@users.noreply.github.com>
2023-12-24 12:11:28 +0500

    Add files via upload

---
+++ class work.py
@@ -0,0 +1,111 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Dell 5490
+#
+# Created:     20/12/2023
+# Copyright:   (c) Dell 5490 2023
+# Licence:     <your licence>
+#----------------------------------------------------------------------------
+num1 = (input)("enter the number")
+num2= (input("enter the number ")
+print= ("addition is commantative"(num1+num2=Num2+Num1))
+
+
+
+
+
+num1= (input("enter the number"))
+num2= (input("enter the number"))
+num3= (input("enter the number"))
+print= ("addition is associative"(num1+(num2=num3)=(num1+num)(num3.))
+
+
+
+
+a= (int("enter the number:"))
+b= (int("enter the number:"))
+print("atleat one number ia possitive")
+
+
+
+a= (int("enter the number:"))
+b= (int("enter the number:"))#
+print("atleast one number is even",a % 2 = 0 or b% 2==00)
+
+a= int("enter the number:")
+b= int("enter the number:")
+print("atleast one number is odd,a% 3 =! 0 or b% 3==0)
+
+
+
+num1=(int("Enter the number"))
+if num1%2==0:
+print("even")
+else:
+elif %3!=0
+print("odd")
+else
+print("zero)
+
+
+
+user_input = (input("enter a character:")
+#check if the input is a single character
+if len(user_input) == 1 :
+if user_input.lowest()in['a', 'e' , 'i', 'o', 'u']:
+  print(f"{user_input}is a vowel.")
+ele:
+   print(f"{user_input}is a consonant.")
+else:
+  print("please enter a single character.")
+
+
+
+
+
+
+age= (input("enter age")
+employment= (input("enter employment status")
+marital=(input("enter marital status")
+if age < 25:
+print("Insurance plan not allowed")
+elif age > age 25 and age < 40 employ="Unemployed:martial status== unmarried"
+print(insurance plan 1400 pkr/month)
+elif age>25 and age <40 employment ==employed :marital status==unmarried
+print("insurance plan 800 pkr/month")
+elif age >25 and age <40 employment==unemployed martial status==married
+print("insurance plan 1200 pkr/month")
+elif age>age 25 and age <40 employment ==employed
+marital status == married
+print("insurance plan 100 pkr/month")
+elif age >40 employment ==any marital status ==any
+printI(insurance 1500 pkr/month)
+
+
+
+
+obtained=int(input("enter the obtained marks:")
+total=int(input("Enter total maks:")
+result =obtained/total*100
+if result >
+print("your grade is A+")
+elif result <88 and result>78:
+print("Your grade is A")
+elif result <78 and result 60:
+print("your grade is B")
+elif result <60 and result > 58:
+print("your grade is C")
+elif result<50 and result >48
+print("your grade is D")
+else:
+print("fail")
+
+
+
+
+
+
+
+
